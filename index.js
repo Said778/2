e711fae3c0e6c07c0c54e42cf75f9fa3693c04ac
Said778 <122379429+Said778@users.noreply.github.com>
2023-10-20 20:18:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let image = document.getElementById('image-5');
+let openButton = document.getElementById('text1111');
+
+
+image.addEventListener('click', function() {
+    openButton.click(); // Симулировать клик на кнопку #open
+});
